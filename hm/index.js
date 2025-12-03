23c909faadd60a55ba61dc4564beef67388b6693
Bianeta <bianetaugwu@gmail.com>
2025-12-03 18:53:40 -0500

    started js

---
+++ hm/index.js
@@ -0,0 +1,11 @@
+//const
+const container= document.getAnimations("alpahabetButtons");
+var answerDisplay = document.getElementById ("hold");
+var answer = "";
+var hint = "";
+var life = 10;
+var wordDisplay= [];
+var winningCheck = "";
+const containerHint= document.getElementById("clue");
+const buttonHint = document.getElementById ("reset");
+const buttonReset =  document.getElementById("mylives");
