54c50303808e61584883af4ef4c8046d8ca018af
Bianeta <bianetaugwu@gmail.com>
2025-12-04 18:52:22 -0500

    almost done with js

--- hm/index.js
+++ hm/index.js
@@ -9,3 +9,42 @@ var winningCheck = "";
 const containerHint= document.getElementById("clue");
 const buttonHint = document.getElementById ("reset");
 const buttonReset =  document.getElementById("mylives");
+var myStickman=document.getElementById("stickman");
+var context = myStickman.getContext("2d");
+
+//generate alphabet buttton
+function generateButton () {
+var buttonsHTML = "abcdefghijklmnopqrstuvwxyz"
+.split("")
+.map(
+    (letter)=>
+        `<button
+         class = "alphabetButtonJS" 
+         id="${letter}"
+         >
+        ${letter}
+               </button>`
+
+)
+.join("");
+
+return buttonsHTML;
+}
+
+function handleClick(event) {
+  const isButton = event.target.nodeName === "BUTTON";
+  if (isButton) {
+    //console.dir(event.target.id);
+    //console.log(isButton);
+    const buttonId = document.getElementById(event.target.id);
+    buttonId.classList.add("selected");
+  }
+  return;
+}
+
+//word array
+const question =[
+    "The Chosen Catagory is Music Artist",
+ "The  Chosen Catagory is Films",
+ "The Chosen Catagory is Books",
+]
